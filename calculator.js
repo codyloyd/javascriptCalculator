2d8f69083df9e910cb472cd107e359b31cf3d801
Cody Loyd <codyloyd@gmail.com>
2016-07-28 13:34:11 -0500

    "works.. but buggy"

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 function add(x,y) {
-  return x + y
-};
+  return x+y
+}
 
 function multiply(x,y) {
   return x*y
@@ -14,8 +14,39 @@ function subtract(x,y) {
   return x - y
 };
 
+function performOperation(calculatorStack, operator) {
+  if (operator == "+") {
+    return add(calculatorStack[calculatorStack.length-2],calculatorStack[calculatorStack.length-1]);
+  } else if (operator == "-") {
+    return subtract(calculatorStack[calculatorStack.length-2],calculatorStack[calculatorStack.length-1]);
+  } else if (operator == "×"){
+    return multiply(calculatorStack[calculatorStack.length-2],calculatorStack[calculatorStack.length-1]);
+  } else if (operator == "÷") {
+    return divide(calculatorStack[calculatorStack.length-2],calculatorStack[calculatorStack.length-1]);
+  }
+};
+
+var calculatorStack = []
+var userIsEnteringNumber = true
+var temporaryWorkingNumber = 0
+var operator = ""
+
 $(document).ready(function(){
   $(".button").click(function(){
-    console.log(this.innerHTML)
+    var input = this.innerHTML
+    if (input === "=") {
+      calculatorStack.push(temporaryWorkingNumber)
+      calculatorStack.push(performOperation(calculatorStack,operator))
+      console.log(calculatorStack)
+      temporaryWorkingNumber = 0
+    } else {
+      if (!isNaN(parseInt(input))) {
+        temporaryWorkingNumber = (temporaryWorkingNumber * 10) + parseInt(input)
+      } else {
+        calculatorStack.push(temporaryWorkingNumber)
+        operator = input
+        temporaryWorkingNumber = 0
+      }
+    }
   });
 });
